65d62209381615ff9bb16f65d8e74e97394ec3bb
Morgikc <144792250+Morgikc@users.noreply.github.com>
2024-05-19 19:16:46 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,1 +1,43 @@
+#include <proxy.h>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+#include <sstream>
 
+
+TEST(tests, test1) {
+    std::ostringstream oss;
+    std::streambuf* cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+    SchoolGirl* girl = new SchoolGirl();
+    girl->setName("A");
+    Proxy* proxy = new Proxy(girl);
+    proxy->GiveDolls();
+    std::cout.rdbuf(cout_streambuf);
+    EXPECT_EQ(oss.str(), "A: give you a doll\n");
+}
+
+TEST(tests, test2) {
+    std::ostringstream oss;
+    std::streambuf* cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+    SchoolGirl* girl = new SchoolGirl();
+    girl->setName("A");
+    Proxy* proxy = new Proxy(girl);
+    proxy->GiveFlowers();
+    std::cout.rdbuf(cout_streambuf);
+    EXPECT_EQ(oss.str(), "A: give you flowers\n");
+}
+
+TEST(tests, test3) {
+    std::ostringstream oss;
+    std::streambuf* cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+    SchoolGirl* girl = new SchoolGirl();
+    girl->setName("B");
+    Proxy* proxy = new Proxy(girl);
+    proxy->GiveDolls();
+    proxy->GiveFlowers();
+    std::cout.rdbuf(cout_streambuf);
+    EXPECT_EQ(oss.str(), "B: give you a doll\nB: give you flowers\n");
+}
